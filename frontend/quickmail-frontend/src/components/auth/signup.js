4b6e56112e7f2b4055f05cc0d6c6a177314a6637
Deep shah <deepjshah.1998@gmail.com>
2024-03-28 04:03:39 -0400

    Sign up completed

--- frontend/quickmail-frontend/src/components/auth/signup.js
+++ frontend/quickmail-frontend/src/components/auth/signup.js
@@ -1,10 +1,98 @@
-import { Container } from "@mui/material";
+import LockOutlined from "@mui/icons-material/LockOutlined";
+import { Avatar, Box, Button, Checkbox, Container, FormControlLabel, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 
 const SignUp = () =>{
     return(
+        
+        <Container component= "main" maxWidth="xs">
+            
+            <Box sx={{
+                marginTop: 8,
+                display:"flex",
+                flexDirection:"column",
+                alignItems:'center',
+                
 
-        <Container component= "main">
+            }}
+            >
+                <Avatar sx={{m:1, backgroundColor:"purple"}}>
+                <LockOutlined/>
+                </Avatar>
+                <Typography component="h1">
+                    Sign Up
+                </Typography>
+
+                <Box component="form" noValidate sx={{ mt:3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField 
+                            autoComplete="given-name"
+                            name="first-name"
+                            label="First Name"
+                            id="firstname"
+                            fullWidth
+                            required
+                            autoFocus                            
+                            />                            
+                        </Grid>
+
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField 
+                            label="Last Name"
+                            id="lastname"
+                            name="last-name"
+                            fullWidth
+                            autoFocus
+                            autoComplete="given-name"
+                            required                            
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField 
+                            name="password"
+                            type="password"
+                            label="Password"
+                            id="password"
+                            required
+                            autoFocus
+                            fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                            name="email"
+                            label="Email"
+                            id="email"
+                            type="email"
+                            required
+                            fullWidth  
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel 
+                            control={<Checkbox value="true" />}
+                            label="Sign up for latest new and information from 
+                            from @ team"
+ 
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Button 
+                    fullWidth
+                    type="submit"
+                    
+                    variant="contained"
+                    sx={{ mt:3, mb:3}}
+                    >
+                        submit
+                        </Button>
+                </Box>
+            </Box>
             
 
 
--- frontend/quickmail-frontend/src/routes.js
+++ frontend/quickmail-frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import login from "./components/auth/login";
+import SignUp from "./components/auth/signup";
 
 
 const Routers = () =>{
@@ -8,7 +9,7 @@ const Routers = () =>{
         <Router>
             <Routes>
                 <Route path="/" exact Component={login} />
-              
+                <Route path="/signup" exact Component={SignUp}/>              
             </Routes>
         </Router>
     );
