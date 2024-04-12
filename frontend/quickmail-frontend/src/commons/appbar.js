57ae7a7e29d54e15dba2a25b70696d2ecce5da04
Deep shah <deepjshah.1998@gmail.com>
2024-04-12 02:12:23 -0400

    updating menu

--- frontend/quickmail-frontend/src/commons/appbar.js
+++ frontend/quickmail-frontend/src/commons/appbar.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Container, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Appbar = () =>{
+    const[open,setOpen] = useState(false);
     return(
         <AppBar position='static' style={{zIndex: 1000}} >
             <Container maxWidth="false" disableGutters>
             <Toolbar variant='dense'>
+                <IconButton onClick={() => setOpen(true)}>
+                    <MenuIcon />
+                </IconButton>
                 <Typography variant='h6' color='inherit'>
                     QuickMail
                 </Typography>
+                
             </Toolbar>
             </Container>
         </AppBar>
